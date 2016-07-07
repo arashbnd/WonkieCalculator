39afdec4db1a6e06dae30c0a904680fa36626925
Grond <grond66@riseup.net>
2016-07-07 13:25:17 -0700

    clean up evaluate.[ch]

--- evaluate.c
+++ evaluate.c
@@ -1,27 +1,27 @@
+
+#include <stdio.h> // for size_t
+
 #include "expression.h"
 
 number_t eval(struct expression_node *node){
    if(node->type == EXPR_LITERAL)
       return node->expr.literal;
-}
-else {
-   
-   switch (node.operator.operation){
-      case OPER_MULT:
-      return eval(node.operator.lhs)*eval(node.operator.rhs);
- 
-      case OPER_DIV:
-      return eval(node.operator.lhs)/eval(node.operator.rhs);
+   else {
+      switch (node->expr.operator.operation){
+         case OPER_MULT:
+         return eval(node->expr.operator.lhs)*eval(node->expr.operator.rhs);
 
-      case OPER_ADD:
-      return eval(node.operator.lhs)+eval(node.operator.rhs);
+         case OPER_DIV:
+         return eval(node->expr.operator.lhs)/eval(node->expr.operator.rhs);
 
-      case OPER_SUBTR:
-      return eval(node.operator.lhs)-eval(node.operator.rhs);
+         case OPER_ADD:
+         return eval(node->expr.operator.lhs)+eval(node->expr.operator.rhs);
 
-      case OPER_NEGATE:
-      return -eval(node.operator.rhs);
+         case OPER_SUBTR:
+         return eval(node->expr.operator.lhs)-eval(node->expr.operator.rhs);
+
+         case OPER_NEGATE:
+         return -eval(node->expr.operator.rhs);
+      }
    }
-
-
 }
